d04114bbecf65b9baf4dc14f8fddb8b8a56cb5b3
halilibrahimozturk <halilibraahimozturk@gmail.com>
2022-06-07 08:52:16 +0300

    06.06.2022 08.52

--- src/main/java/com/halilibrahimozturk/test/TestDbServlet.java
+++ src/main/java/com/halilibrahimozturk/test/TestDbServlet.java
@@ -1,10 +1,15 @@
 package com.halilibrahimozturk.test;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 
 public class TestDbServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -12,9 +17,37 @@ public class TestDbServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String htmlResponse = "<html>";
-        htmlResponse += "<h3>Hello World in Servlet</h3>";
-        htmlResponse += "</html>";
-        response.getWriter().println(htmlResponse);
+
+        // setup connection variables
+
+        String user = "springstudent";
+        String password = "springstudent";
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+
+        // get connection to database
+
+        try {
+            PrintWriter out = response.getWriter();
+
+            out.println("Connecting to database: " + jdbcUrl);
+
+            Class.forName(driver);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, password);
+
+            out.println("Connection Succesfull!");
+
+            myConn.close();
+        } catch (Exception exc) {
+
+            exc.printStackTrace();
+            throw new ServletException(exc);
+        }
+
+
     }
 }
